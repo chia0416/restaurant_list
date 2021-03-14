279f6e3372e6831c52ff00e456e1311542728a0b
chia hung <a0926223478@hotmail.com>
2021-03-14 15:02:01 +0800

    restaurant_list
    S3_W1_A3: 打造餐廳清單

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    if (restaurant.name.toLowerCase().includes(keyword.toLowerCase())) {
+      return restaurant.name;
+    } else if (
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    ) {
+      return restaurant.category;
+    }
+  });
+
+  res.render("index", { restaurants: restaurants });
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
