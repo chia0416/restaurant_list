9f65c50fbee65dbb70142c27f370b4b6e64699ef
chia hung <a0926223478@hotmail.com>
2021-03-21 22:45:55 +0800

    feat restaurant_list_v2

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const RestaurantList = require("./models/restaurant");
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}) )
 
 mongoose.connect("mongodb://localhost/restaurant-list", {
   useNewUrlParser: true,
@@ -25,6 +28,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
+//show index
 app.get("/", (req, res) => {
   RestaurantList.find()
   .lean()
@@ -32,6 +36,7 @@ app.get("/", (req, res) => {
   .catch(error => console.log(error))
 });
 
+//show detail
 app.get("/restaurants/:id", (req, res) => {
   const id = req.params.id
   return RestaurantList.findById(id)
@@ -41,6 +46,7 @@ app.get("/restaurants/:id", (req, res) => {
 
 });
 
+//搜尋
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword;
 
@@ -52,11 +58,10 @@ app.get("/search", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-
-//此處路由器讀不到 /index.handlebars的modal
+//新增
 app.post('/create', (req, res) => {
   const { name, category, image, location, phone, google_map, rating, description } = req.body
-  
+  console.log('created')
   return RestaurantList.create({
     name: name,
     category: category,
@@ -70,6 +75,44 @@ app.post('/create', (req, res) => {
     .then(res.redirect('/'))
     .catch(error => console.log(error))
 })
+
+//show edit
+app.get("/restaurant/edit/:id", (req, res) => {
+  const id = req.params.id;
+  return RestaurantList.findById(id)
+    .lean()
+    .then((restaurantList) => res.render("edit", { restaurantList }))
+    .catch((error) => console.log(error));
+});
+
+//編輯要求
+app.post("/restaurants/edit/:id", (req, res) => {
+  const { name, category, image, location, phone, google_map, rating, description } = req.body
+  const id = req.params.id;
+  return RestaurantList.findById(id)
+    .then((restaurants) => {
+      restaurants.name = name;
+      restaurants.phone = phone;
+      restaurants.category = category;
+      restaurants.rating = rating;
+      restaurants.location = location;
+      restaurants.image = image;
+      restaurants.google_map = google_map;
+      restaurants.description = description;
+      return restaurants.save();
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.log(error));
+});
+
+//刪除
+app.post("/restaurants/:id/delete", (req, res) => {
+  const id = req.params.id;
+  return RestaurantList.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
 app.listen(3000, () => {
   console.log("Express is listening on localhost:3000");
