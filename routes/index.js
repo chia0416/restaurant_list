1f062c5cabc4f774b6d477a02b5f8b26baccc43a
chia hung <a0926223478@hotmail.com>
2021-05-30 21:49:17 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const home = require("./modules/home");
 const restaurants = require("./modules/restaurants");
 const sort = require('./modules/sort')
+const users = require('./modules/user')
 
 router.use("/", home);
 router.use("/restaurants", restaurants);
 router.use("/sort", sort)
+router.user('/users', users)
 
 module.exports = router;
