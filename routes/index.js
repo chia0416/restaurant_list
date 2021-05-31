c15bc2c5844167c179290f29f1021ac6edb13248
chia hung <a0926223478@hotmail.com>
2021-05-31 13:09:21 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const home = require("./modules/home");
 const restaurants = require("./modules/restaurants");
 const sort = require('./modules/sort')
-const users = require('./modules/user')
+const users = require('./modules/users')
 
 router.use("/", home);
 router.use("/restaurants", restaurants);
 router.use("/sort", sort)
-router.user('/users', users)
+router.use('/users', users)
 
 module.exports = router;
