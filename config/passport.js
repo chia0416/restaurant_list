ddc9f4ce048bc6e28bfa2a8ed889349237ccbd10
chia hung <a0926223478@hotmail.com>
2021-08-15 23:44:04 +0800

    feat: add login checking message before POST

--- config/passport.js
+++ config/passport.js
@@ -1,66 +1,85 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const FacebookStrategy = require('passport-facebook').Strategy
-const bcrypt = require('bcryptjs')
-const User = require('../models/user')
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 module.exports = (app) => {
-  app.use(passport.initialize())
-  app.use(passport.session())
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true,
-    session: false
-  }, (req, email, password, done) => {
-    User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          return done(null, false, req.flash('warning_msg', 'That email is not registered!'))
-        }
-        return bcrypt.compare(password, user.password).then(isMatch => {
-          if (!isMatch) {
-            return done(null, false, req.flash('error_msg', 'Email or Password incorrect.'))
-          }
-          return done(null, user)
-        })
-      })
-      .catch(err => done(err, false))
-  }))
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true,
+        session: false,
+      },
+      (req, email, password, done) => {
+        User.findOne({ email })
+          .then((user) => {
+            if (!user) {
+              return done(
+                null,
+                false,
+                req.flash("warning_msg", "這個信箱尚未註冊")
+              );
+            }
+            return bcrypt.compare(password, user.password).then((isMatch) => {
+              if (!isMatch) {
+                return done(
+                  null,
+                  false,
+                  req.flash("warning_msg", "帳號或密碼錯誤")
+                );
+              }
+              return done(null, user);
+            });
+          })
+          .catch((err) => done(err, false));
+      }
+    )
+  );
 
-  passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK,
-    profileFields: ['email', 'displayName']
-  }, (accessToken, refreshToken, profile, done) => {
-    const { name, email } = profile._json
-    User.findOne({ email })
-      .then(user => {
-        if (user) return done(null, user)
-        const randomPassword = Math.random().toString(36).slice(-8)
-        bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash(randomPassword, salt))
-          .then(hash => User.create({
-            name,
-            email,
-            password: hash
-          }))
-          .then(user => done(null, user))
-          .catch(err => done(err, false))
-      })
-  }))
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ["email", "displayName"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user);
+          const randomPassword = Math.random().toString(36).slice(-8);
+          bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(randomPassword, salt))
+            .then((hash) =>
+              User.create({
+                name,
+                email,
+                password: hash,
+              })
+            )
+            .then((user) => done(null, user))
+            .catch((err) => done(err, false));
+        });
+      }
+    )
+  );
 
   passport.serializeUser((user, done) => {
-    console.log('user:', user)
-    done(null, user.id)
-  })
+    console.log("user:", user);
+    done(null, user.id);
+  });
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean()
-      .then(user => done(null, user))
-      .catch(err => done(err, null))
-  })
-}
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null));
+  });
+};
